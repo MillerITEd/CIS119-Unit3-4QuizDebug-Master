5893138b24e95784ad0861d668b024ab87507afd
Kelli Miller <49914948+MillerITEd@users.noreply.github.com>
2020-10-22 16:14:26 -0500

    Initial commit

---
+++ public_html/debug.js
@@ -0,0 +1,18 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//This program computes the sum of 2 given integers.
+//If the two values are the same, then it returns triple their sum.
+
+var number1 = prompt("Enter the first number");
+var number2 = prompt("Enter the second number")
+
+var sum = number1 + number2;
+if (number1 = number2) {
+    document.write(3 * sum);
+}
+else 
+    document.write (sum);
+}
